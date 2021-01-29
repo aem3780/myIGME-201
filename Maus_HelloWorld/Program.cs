681213952e31a9019c4291455c70bc3b3c59d2b5
Allie Maus <aem3780@rit.edu>
2021-01-29 17:16:25 -0500

    PE1

---
+++ Maus_HelloWorld/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maus_HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Allie Maus");
+
+            for(int num = 0; num <12; ++num)
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
