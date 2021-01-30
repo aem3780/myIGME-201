d782727d8e67350da64b8cbaee677b088aae76e9
Allie Maus <aem3780@rit.edu>
2021-01-30 14:36:34 -0500

    Corrected homework 1

--- Maus_HelloWorld/Program.cs
+++ Maus_HelloWorld/Program.cs
@@ -6,14 +6,24 @@ using System.Threading.Tasks;
 
 namespace Maus_HelloWorld
 {
-    class Program
+    //Class: Program
+    //Author: Allie Maus
+    //Purpose: Create a console application and experiment with c# syntax
+    //Restrictions: none
+    static class Program
     {
+        //Method: Main
+        //Purpose: To print out Hello World to console and experiment with c# Syntax
+        //Restrictions: none
+
         static void Main(string[] args)
         {
+            //Prints hello world to console
             Console.WriteLine("Hello World!");
-
+            //Prints my name to console
             Console.WriteLine("Allie Maus");
 
+            //for loop that prints numbers 0-11 to console
             for(int num = 0; num <12; ++num)
             {
                 Console.WriteLine(num);
