ab55e3e6771ddd2547711a4028f43b1b4bd6e379
Allison Maus <allison_1317@aol.com>
2021-02-07 14:45:02 -0500

    PE4 exercise 1, I can't seem to get this working right but I gave it my best shot

---
+++ PE4_Exercise1/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE4_Exercise1
+{
+    //Class: program
+    //Author: Allison Maus
+    //Purpose: PE4 exercise 1
+    //Restrictions: none
+    static class Program
+    {
+        //Method: main
+        //Purpose: accept two numbers from the user and display them
+        //on screen, but reject the numbers if both are greater than 10
+        //Restrictions: does not reach the catch in try catch, and does not work
+        //if one number greater then 10 is entered
+        static void Main(string[] args)
+        {
+            //initialize and declare variables
+            int value1 = 0;
+            int value2 = 0;
+            bool valid = false;
+
+            //asks the user to input two numbers while valid is false
+            do
+            {
+                //ask user for input and convert the string to an integer
+                Console.WriteLine("Please enter your first number:");
+                value1 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Please enter your second number:");
+                value2 = Convert.ToInt32(Console.ReadLine());
+                //try the comparison 
+                try
+                {
+                    //if value1 is less than or equal to 10 and value 2 is less than
+                    //or equal to 10 set valid to true
+                    if((value1 <= 10)&(value2 <= 10))
+                    {
+                        valid = true;
+                    
+                    }
+                    
+              
+                }
+                //print to console if try fails
+                catch
+                {
+                    Console.WriteLine("Please enter a new numbers");
+                }
+
+            }
+            while (!valid);
+
+            //display numbers on the console
+            Console.WriteLine("Here are your numbers: {0} {1}", value1, value2);
+
+           
+
+            
+        }
+    }
+}
--- practice/Program.cs
+++ practice/Program.cs
@@ -10,32 +10,28 @@ namespace practice
     {
         static void Main(string[] args)
         {
-            string myName = "Allie";
-            string lastName = "Maus";
-            Console.WriteLine(myName + lastName);
-
-            int num1 = 2;
-            int num2 = 4;
-            int num3 = 5;
-            num1 = num1 * num3;
-            num2 = num1 / num2;
-            num3 = num3 % num2;
-
-            Console.WriteLine(num1);
-            Console.WriteLine(num2);
-            Console.WriteLine(num3);
-
-            double num4 = (6 / 4.0 + 3.5) / 2;
-            Console.WriteLine(num4);
-
-            int $bankBalance = 8;
-            int Factorial! = 9;
-            int black&white = 9;
-
-
-
+            string favoriteFood = "spaghetti";
+            switch (favoriteFood)
+            {
+                case "spaghetti":
+                    Console.WriteLine("You seem to really like spaghetti");
+                    break;
+                case "cake":
+                    Console.WriteLine("Wrong. Spaghetti is the best food.");
+                    break;
+                default:  
+                    Console.WriteLine("You should like spaghetti.");
+                    break;
+            }
 
 
         }
+
+
+
+
+
+
     }
-}
+    }
+
