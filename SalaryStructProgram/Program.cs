278c4ea1d38dd26a9555c271698e28436e320c61
Allison Maus <allison_1317@aol.com>
2021-02-23 18:30:43 -0500

    Unit Test 1 Question 13

--- SalaryStructProgram/Program.cs
+++ SalaryStructProgram/Program.cs
@@ -6,19 +6,23 @@ using System.Threading.Tasks;
 
 namespace SalaryStructProgram
 {   
-    struct employee
+    struct Employee
         {
+            // member variables with defined accessibility
             public string sName;
             public double dSalary;
 
-            public bool GiveRaise()
-            {
+            // member property for accessing the GiveRaise function
+           public bool GiveRaise()
+           {
+            //if sName is allie
             if(this.sName.ToLower()=="allie")
             {
                 //add 19999.99 to current salary and return true
                 this.dSalary += 19999.99;
                 return true;
             }
+            //if sName is anything else
             else
             {
                 return false;
@@ -26,17 +30,28 @@ namespace SalaryStructProgram
 
         }
         }
-    class Program
+
+    //Class: Program
+    //Author: Allison Maus
+    //Purpose: Unit Test 1 
+    //Restrictions: none
+    static class Program
     {
+        //Method: Main
+        //Purpose: Use a struct to recreate question 12
+        //Restrictions: none
         static void Main(string[] args)
         {
 
+            //create new Employee
+            Employee person = new Employee();
 
-            employee person = new employee();
-
+            //assign allie sName
             person.sName = "Allie";
+            //assign 30000 to dSalary
             person.dSalary = 30000;
 
+            //if GIveRaise is true print congrats to console and new salary
             if (person.GiveRaise()==true)
             {
                 Console.WriteLine("Congratulations on your raise! Your new salary is ${0}.", person.dSalary);
