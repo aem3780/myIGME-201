b2adb19028c6a95590a13d8ffd27284a06303f64
Allison Maus <allison_1317@aol.com>
2021-04-09 17:34:01 -0400

    GifFinder PE24

---
+++ GifFinder/Form2.Designer.cs
@@ -0,0 +1,120 @@
+﻿
+namespace GifFinder
+{
+    partial class SearchForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.searchItemTextBox = new System.Windows.Forms.TextBox();
+            this.maxItemstextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.okButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // searchItemTextBox
+            // 
+            this.searchItemTextBox.Location = new System.Drawing.Point(85, 31);
+            this.searchItemTextBox.Name = "searchItemTextBox";
+            this.searchItemTextBox.Size = new System.Drawing.Size(100, 20);
+            this.searchItemTextBox.TabIndex = 0;
+            // 
+            // maxItemstextBox
+            // 
+            this.maxItemstextBox.Location = new System.Drawing.Point(85, 66);
+            this.maxItemstextBox.Name = "maxItemstextBox";
+            this.maxItemstextBox.Size = new System.Drawing.Size(100, 20);
+            this.maxItemstextBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Search Item:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Max Items:";
+            // 
+            // okButton
+            // 
+            this.okButton.Location = new System.Drawing.Point(15, 131);
+            this.okButton.Name = "okButton";
+            this.okButton.Size = new System.Drawing.Size(75, 23);
+            this.okButton.TabIndex = 4;
+            this.okButton.Text = "OK";
+            this.okButton.UseVisualStyleBackColor = true;
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.cancelButton.Location = new System.Drawing.Point(110, 131);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(75, 23);
+            this.cancelButton.TabIndex = 5;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            // 
+            // SearchForm
+            // 
+            this.AcceptButton = this.okButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.cancelButton;
+            this.ClientSize = new System.Drawing.Size(211, 182);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.okButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.maxItemstextBox);
+            this.Controls.Add(this.searchItemTextBox);
+            this.Name = "SearchForm";
+            this.Text = "Search";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox searchItemTextBox;
+        private System.Windows.Forms.TextBox maxItemstextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button okButton;
+        private System.Windows.Forms.Button cancelButton;
+    }
+}
---
+++ GifFinder/Form2.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GifFinder
+{
+    public partial class SearchForm : Form
+    {
+
+        public string response;
+        public string searchTerm;
+        public int maxItems;
+
+        public SearchForm()
+        {
+            InitializeComponent();
+
+            this.maxItemstextBox.KeyPress += new KeyPressEventHandler(MaxItemstextBox__KeyPress);
+            this.okButton.Click += new EventHandler(OkButton__Click);
+            this.cancelButton.Click += new EventHandler(CancelButton__Click);
+        }
+
+        private void MaxItemstextBox__KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = true;
+            if(Char.IsDigit(e.KeyChar)|| e.KeyChar == '\b')
+            {
+                e.Handled = false;
+            }
+        }
+
+        private void OkButton__Click(object sender, EventArgs e)
+        {
+            this.response = "OK";
+
+            this.searchTerm = searchItemTextBox.Text;
+            maxItems = Convert.ToInt32(maxItemstextBox.Text);
+            this.Hide();
+        }
+
+        private void CancelButton__Click(object sender, EventArgs e)
+        {
+            this.response = "Cancel";
+            this.Hide();
+        }
+
+    }
+}
